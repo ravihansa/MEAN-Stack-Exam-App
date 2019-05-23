599806161e5dc64420d013717cdbc4c45dd2c3fd
ravihansa <ravihansad@gmail.com>
2019-05-23 14:53:12 +0530

    Create user authorization back end

--- NodeJS-Backend/controllers/user.controller.js
+++ NodeJS-Backend/controllers/user.controller.js
@@ -35,3 +35,14 @@ module.exports.authenticate = (req, res, next) => {
         else return res.status(404).json(info);
     })(req, res);
 }
+
+module.exports.userProfile = (req, res, next) => {
+    User.findOne({ _id: req._id },
+        (err, user) => {
+            if (!user)
+                return res.status(404).json({ status: false, message: 'User record not found.' });
+            else
+                return res.status(200).json({ status: true, user : _.pick(user,['firstName','email']) });
+        }
+    );
+}
